6fd1123ee43eca6e64a312201883ab0755bccb2e
chaeyeon22_ <pink5465y@gmail.com>
2023-07-28 02:09:32 +0900

    [Bronze V] Title: 개, Time: 192 ms, Memory: 17332 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+    }
+}
