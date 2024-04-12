bf5a552eb0131b58d7a35b4c2ffc183c6f356120
chaeyeon22_ <pink5465y@gmail.com>
2024-04-12 20:57:13 +0900

    [Silver V] Title: 숫자 카드, Time: 3676 ms, Memory: 113340 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(input())
+ns = list(map(int, sys.stdin.readline().split()))
+m = int(input())
+ms = list(map(int, sys.stdin.readline().split()))
+result = []
+
+ns.sort()
+
+for i in range(m):
+    start = 0
+    end = n-1
+    a=0
+    while (start <= end):
+        mid = (start+end) // 2
+        if ms[i] == ns[mid]:
+            a = 1
+            break
+        elif ms[i]>ns[mid]:
+            start = mid +1
+        else:
+            end = mid-1
+    if a==1:
+        print(1, end=" ")
+    else:
+        print(0, end=" ")
