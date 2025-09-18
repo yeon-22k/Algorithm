15ab4c74e25c9906e19e1a87f928b8a49a7a6eff
yeonDy <pink5465y@gmail.com>
2025-09-18 10:16:40 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = (num1 * 1000) / num2;
+        return answer;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        
+        Solution s = new Solution();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(s.solution(num1, num2));
+        bw.flush();
+        bw.close();
+    }
+}
